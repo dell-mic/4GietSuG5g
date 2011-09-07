e4e111849a044617e401ddc1aa1b28d1c195cab8
speedy891 <konstantin.heine@googlemail.com>
2011-09-07 08:22:03 +0200

    First commit

---
+++ src/init_only/HelloWorld.java
@@ -0,0 +1,15 @@
+package init_only;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
