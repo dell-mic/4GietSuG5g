96690d4968698323f9b7ae37db9d5e0aea7948e3
toni220506 <antonpeters22@googlemail.com>
2011-09-20 10:27:52 +0200

    hier gehts los

---
+++ src/ui/A_UIHistorie.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class A_UIHistorie {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ui/A_UISpiel.java
@@ -0,0 +1,25 @@
+package ui;
+
+import javax.swing.*;
+
+public class A_UISpiel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		A_UISpiel gui = new A_UISpiel();
+		gui.start();
+
+	}
+
+	private void start() {
+		
+		JFrame frMain = new JFrame();
+		
+		
+	}
+
+}
