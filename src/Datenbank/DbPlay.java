d680063cd8d801bb545dcead3dd65643382c8dbd
gini <jenny.geipel@googlemail.com>
2011-09-20 21:43:30 +0200

    Tabellen erzeugen

--- src/Datenbank/DbPlay.java
+++ src/Datenbank/DbPlay.java
@@ -1,17 +1,18 @@
 package Datenbank;
 
-import java.sql.Connection;
+import java.sql.Connection; // Datenbankverbindung
+import java.sql.Statement; // ermöglicht SQL Abfragen
 import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
+import java.sql.DriverManager; //Einstiegspunkt
 import java.sql.SQLException;
-import java.sql.ResultSet;
+import java.sql.ResultSet; //Ergebnisverwaltung
 
 public class DbPlay {
 
 	private Connection dbConnection;
-	
+	private Statement dbStatement;	
 	private final String PATH = System.getProperty("user.dir") + "\\get4db";
-	
+	private String driverClass = "org.hsqldb.jdbcDriver";
 
 	
 	/**
@@ -24,8 +25,16 @@ public class DbPlay {
 		
 		
 		// Datenbankverbindung initiieren
-		Class.forName("org.hsqldb.jdbcDriver");
-		dbConnection = DriverManager.getConnection("jdbc:hsqldb:file:" + PATH, "sa", "");
+		try{
+		Class.forName(driverClass);
+		}
+		catch(ClassNotFoundException exc) {
+			System.out.println(exc.getMessage());
+			System.exit(1);
+		}
+		
+		
+		dbConnection = DriverManager.getConnection("jdbc:hsqldb:file:" + PATH, "sa", "");  //Verbindungsaufbau
 		
 		// Prüfen, ob DB schon existiert
 		// falls die Datenbank leer ist, Tabellen erstellen
@@ -50,18 +59,103 @@ public class DbPlay {
 			return false;
 
 		// kein Datensatz vorhanden, demnach ist auch keine Tabelle vorhanden
-		System.out.println("keine Tabellen vorhanden");
 		return true;
 	}
 	
 	/**
-	 * Falls DB leer werde hier die Tabellen erzeugt und Daten eingefügt
+	 * Falls DB leer - werde hier die Tabellen erzeugt
 	 */
 	private synchronized void initDB() {
+	
+		try{
+		// Tabelle für die Spiele
+		statement("CREATE TABLE Game ("
+				+ " GameID INT NOT NULL,"
+				+ " Beginn TIMESTAMP,"
+				+ " End TIMESTAMP,"
+				+ " CONSTRAINT pk_Game PRIMARY KEY (GameID),"
+				+ ");"
+		);
 		
-			// hier Tabellen erzeugen!
+		// Tabelle für die Spieler
+		statement("CREATE TABLE Player ("
+				+ " PlayerID INT NOT NULL,"
+				+ " Name VARCHAR(255),"
+				+ " CONSTRAINT pk_Player PRIMARY KEY (PlayerID),"
+				+ ");"
+		);
+
+		// Tabelle für den Zug
+		statement("CREATE TABLE Move ("
+				+ " MoveID INT NOT NULL,"
+				+ " GameID INT NOT NULL,"
+				+ " RoundID INT NOT NULL,"
+				+ " PlayerID INT NOT NULL,"
+				+ " PositionX INT,"
+				+ " PositionY INT,"
+				+ " CONSTRAINT pk_Move PRIMARY KEY (MoveID, PlayerID, RoundID),"
+				+ " CONSTRAINT fk_Move_Player FOREIGN KEY (PlayerID) PREFERENCES Player (PlayerID),"
+				+ " CONSTRAINT fk_Move_Round FOREIGN KEY (RoundID) PREFERENCES Round (RoundID, GameID),"
+				+ ");"
+		);
+
+		// Tabelle für den Satz
+		statement("CREATE TABLE Round ("
+				+ " GameID INT NOT NULL,"
+				+ " RoundID INT NOT NULL,"
+				+ " Beginn TIMESTAMP,"
+				+ " End TIMESTAMP,"
+				+ " CONSTRAINT pk_Round PRIMARY KEY (GameID, RoundID),"
+				+ " CONSTRAINT fk_Round_Game FOREIGN KEY (GameID) PREFERENCES Game (GameID),"
+				+ ");"
+		);
+		
+		// Tabelle für Spieler spielt Spiel
+		statement("CREATE TABLE PlayerPlaysGame ("
+				+ " GameID INT NOT NULL,"
+				+ " PlayerID INT NOT NULL,"
+				+ " Win INT,"
+				+ " Sign VARCHAR(255),"
+				+ " CONSTRAINT pk_PPG PRIMARY KEY (PlayerID, GameID),"
+				+ " CONSTRAINT fk_PPG_Game FOREIGN KEY (GameID) PREFERENCES Game (GameID),"
+				+ ");"
+		);
+		
+		// Tabelle für Spieler spielt Satz
+		statement("CREATE TABLE PlayerPlaysRound ("
+				+ " GameID INT NOT NULL,"
+				+ " RoundID INT NOT NULL,"
+				+ " PlayerID INT NOT NULL,"
+				+ " Win INT,"
+				+ " Starts INT,"
+				+ " CONSTRAINT pk_PPR PRIMARY KEY (PlayerID, GameID, RoundID),"
+				+ " CONSTRAINT fk_PPR_Game FOREIGN KEY (GameID) PREFERENCES Game (GameID),"
+				+ " CONSTRAINT fk_PPR_Player FOREIGN KEY (PlayerID) PREFERENCES Player (PlayerID),"
+				+ ");"
+		);
+		
+		
+		
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	
 		}
+	
+	/**
+	 * Führt ein beliebiges Update Statement auf der Datenbank aus
+	 * @param SQL Anweisung
+	 * @throws SQLException
+	 */
+	public synchronized void statement(String expression) throws SQLException {
+		int i = dbStatement.executeUpdate(expression);
+
+		if (i == -1) {
+			throw new SQLException("Database error for this statement: " + expression);
+		}
+	}
 
 	}
 	
